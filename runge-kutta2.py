197224578a8062cb2a36626825ba524cccf8e313
Ana Paula Falcão <anafalcao@poli.ufrj.br>
2019-06-21 11:11:11 -0300

    Update runge-kutta2.py

--- runge-kutta2.py
+++ runge-kutta2.py
@@ -1,26 +1,49 @@
-import numpy as np
-
-
-def funcion(x,y):
-    ec=x+y
-    return ec
-
-h=float(input("Tamanho do passo: "))
-s=float(input("Até qual valor? "))
-n=int((s/h)+1)
-
-
-x=np.zeros(n)
-y=np.zeros(n)
-
-
-for i in np.arange(1,n):
-    x[i]=x[i-1]+h
-    K1=funcion(x[i-1],y[i-1])
-    K2=funcion(x[i-1]+h,y[i-1]+(h*K1))
-    y[i]=y[i-1]+(h/2)*(K1+K2)
-print(y[i])
-
-
-
-                       
+import numpy as np
+
+
+def funcion(x,y):
+
+    ec=x+y
+
+    return ec
+
+
+
+h=float(input("Tamanho do passo: "))
+
+s=float(input("Até qual valor? "))
+
+n=int((s/h)+1)
+
+
+
+
+
+x=np.zeros(n)
+
+y=np.zeros(n)
+
+
+
+
+
+for i in np.arange(1,n):
+
+    x[i]=x[i-1]+h
+
+    K1=funcion(x[i-1],y[i-1])
+
+    K2=funcion(x[i-1]+h,y[i-1]+(h*K1))
+
+    y[i]=y[i-1]+(h/2)*(K1+K2)
+
+print(y[i])
+
+
+
+
+
+
+
+                       
+
