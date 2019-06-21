a23faf88ce11a876997b5811506da262b9af5c4c
Ana Paula Falcão <anafalcao@poli.ufrj.br>
2019-06-21 11:11:53 -0300

    Update euler.py

--- euler.py
+++ euler.py
@@ -1,20 +1,36 @@
-import numpy as np
-
-
-def funcion(x,y):
-    ec=x+y
-    return ec
-
-h=float(input("Tamanho do passo: "))
-s=float(input("Até qual valor? "))
-n=int((s/h)+1)
-
-x=np.zeros(n)
-y=np.zeros(n)
-
-
-for i in np.arange(1,n):
-    y[i]=y[i-1]+(funcion(x[i-1],y[i-1]))*h
-    x[i]=x[i-1]+h
-print(y[i])
-
+import numpy as np
+
+def funcion(x,y):
+
+    ec=x+y
+
+    return ec
+
+
+
+h=float(input("Tamanho do passo: "))
+
+s=float(input("Até qual valor? "))
+
+n=int((s/h)+1)
+
+
+
+x=np.zeros(n)
+
+y=np.zeros(n)
+
+
+
+
+
+for i in np.arange(1,n):
+
+    y[i]=y[i-1]+(funcion(x[i-1],y[i-1]))*h
+
+    x[i]=x[i-1]+h
+
+print(y[i])
+
+
+
